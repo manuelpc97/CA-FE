09e570d0e71b3ad1cf0e389b06da9eaf59d5faf5
DanielaOdileth <odilethpadilla@unitec.edu>
2020-05-31 12:27:25 -0600

    adding logout button

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,41 +1,60 @@
 import '../../styles/Sidebar.css';
-
-import React,{Component} from 'react';
-import {List, ListSubheader, Divider} from '@material-ui/core';
-import {Person, Dashboard, LocalGroceryStore} from '@material-ui/icons';
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { List, ListSubheader, Divider } from '@material-ui/core';
+import { Person, Dashboard, LocalGroceryStore, ExitToApp } from '@material-ui/icons';
+import { singUp, changePath } from '../../actions';
 
 import Tab from './Tab';
 
-class Sidebar extends Component{
-    constructor(props){
+class Sidebar extends Component {
+    constructor(props) {
         super(props);
         this.tabs = [
             {
                 text: 'Seguros',
-                icon: <Dashboard/> 
+                icon: <Dashboard />
             },
             {
                 text: 'Perfil',
-                icon: <Person/> 
-            }, 
+                icon: <Person />
+            },
             {
-                text: 'Productos', 
-                icon: <LocalGroceryStore/>
-            }
+                text: 'Productos',
+                icon: <LocalGroceryStore />
+            },
+            
         ]
+        this.logOutTab = {
+            text: 'Cerrar Sesión',
+            icon: <ExitToApp />
+        }
     }
-    render(){
-        return <List subheader = {this.renderSubheader()}>
-            {this.tabs.map((tab, index) => <Tab tab = {tab} key = {index} focused = {this.props.currentIndex === index} index = {index}/>)}
+    logOut = () => {
+        this.props.changePath('')
+    }
+    render() {
+        return <List subheader={this.renderSubheader()}>
+            {this.tabs.map((tab, index) => <Tab tab={tab} key={index} focused={this.props.currentIndex === index} index={index} />)}
+            <div  className='logOuIcon' onClick={this.logOut}>
+                <Tab tab={this.logOutTab} key={'logOut'} focused={false} />
+            </div>
         </List>;
     }
 
     renderSubheader = () => {
         return <ListSubheader>
-            <div className = 'sidebar-subheader'>SEGURU</div>
-            <Divider className = 'sidebar-divider' variant = 'middle'></Divider>
+            <div className='sidebar-subheader'>SEGURU</div>
+            <Divider className='sidebar-divider' variant='middle'></Divider>
         </ListSubheader>
     }
 }
 
-export default Sidebar;
+const mapStateToProps = state => {
+    const { error } = state;
+    return {
+        error
+    }
+}
+
+export default connect(mapStateToProps, { singUp, changePath })(Sidebar)
