4954ec0441ce82fe8db5b7aa37880166a905c5a0
manuelpc97 <mcanelo41@gmail.com>
2020-05-30 21:22:38 -0600

    Getting user from profile component

--- src/components/Content/Profile.js
+++ src/components/Content/Profile.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Profile extends Component{
+    componentDidMount(){
+        console.log('USER: ', this.props.user);
+    }
+
     render(){
         return <div>PROFILE</div>;
     }
 }
 
-export default Profile;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user.currentUser
+    }
+}
+
+export default connect(mapStateToProps, {})(Profile);
