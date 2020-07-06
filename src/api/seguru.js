84f758f42ae8af1e03796259d471f967232f4a83
manuelpc97 <mcanelo41@gmail.com>
2020-07-05 21:30:47 -0600

    Updates

--- src/api/seguru.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL : 'http://shrouded-cove-22974.herokuapp.com'
-});
--- src/components/Form/Label.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class Label extends Component{
-    render(){
-        return <div><h4>{this.props.question.question}</h4></div>
-    }
-}
-
-export default Label;
